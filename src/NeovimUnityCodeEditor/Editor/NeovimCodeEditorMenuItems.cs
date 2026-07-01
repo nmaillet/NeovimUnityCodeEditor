18c2c9fe52a70929364cd3d88bda286bad1ed69b
Nic Maillet <nmaillet@gmail.com>
2026-07-01 11:39:14 -0300

    Update Neovim menu
    - Remove & prefix from menu item labels
    - Add Preferences menu item to open to directly to External Tools/Neovim Code Editor

--- src/NeovimUnityCodeEditor/Editor/NeovimCodeEditorMenuItems.cs
+++ src/NeovimUnityCodeEditor/Editor/NeovimCodeEditorMenuItems.cs
@@ -4,28 +4,34 @@ namespace SigmaTau.Unity.NeovimCodeEditor.Editor
 {
     public static class NeovimCodeEditorMenuItems
     {
-        [MenuItem("&Neovim/Generate Project Files", true)]
+        [MenuItem("Neovim/Generate Project Files", true)]
         public static bool IsGenerateProjectFilesValid()
         {
             return NeovimExternalCodeEditor.Instance.IsCurrent;
         }
 
-        [MenuItem("&Neovim/Generate Project Files", false, 0)]
+        [MenuItem("Neovim/Generate Project Files", false, 0)]
         public static void GenerateProjectFiles()
         {
             NeovimExternalCodeEditor.Instance.SyncAll();
         }
 
-        [MenuItem("&Neovim/Clear Named Pipe", true)]
+        [MenuItem("Neovim/Clear Named Pipe", true)]
         public static bool IsClearNamedPipeValid()
         {
             return NeovimExternalCodeEditor.Instance.HasNamedPipe;
         }
 
-        [MenuItem("&Neovim/Clear Named Pipe", false, 100)]
+        [MenuItem("Neovim/Clear Named Pipe", false, 100)]
         public static void ClearNamedPipe()
         {
             NeovimExternalCodeEditor.Instance.ClearNamedPipe();
+        }
+
+        [MenuItem("Neovim/Preferences", false, 200)]
+        public static void Preferences()
+        {
+            SettingsService.OpenUserPreferences(NeovimCodeEditorSettingsProvider.SettingsPath);
         }
     }
 }
